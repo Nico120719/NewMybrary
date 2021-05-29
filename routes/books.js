a05966d0019aae11a36e5b665279633703f56659
Nico <nicolas.ineksiak@gmail.com>
2021-05-29 02:05:21 -0400

    11:00

--- routes/books.js
+++ routes/books.js
@@ -52,6 +52,7 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Show Book Route
 router.get('/:id', async (req, res) => {
     try {
         const book = await Book.findById(req.params.id).populate('author').exec()
@@ -61,7 +62,25 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+// Edit Book Route
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    } catch {
+        res.redirect('/')
+    }
+})
+
 async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({})
         const params = {
@@ -69,7 +88,7 @@ async function renderNewPage(res, book, hasError = false) {
             book: book
         }
         if (hasError) params.errorMessage = 'Error Creating Book'
-        res.render('books/new', params)
+        res.render(`books/${form}`, params)
     } catch {
         res.redirect('/books')
     }
